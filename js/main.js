20f49ff8a950e64cb0b831684d9240fd3fa22b14
jaga <jagawebdev@gmail.com>
2017-02-08 16:20:36 +0000

    uploaded whole website

---
+++ js/main.js
@@ -0,0 +1,72 @@
+/*global $*/
+/* PRELOADER */
+$(window).ready(function() {
+    $('#loading').hide();
+});
+
+
+$(document).ready(function() {
+   /**
+   * Drop Down Menu
+   */
+    $('.hamburger').click( function(event){
+        event.stopPropagation();
+        $('.drop-menu').toggle();
+    });
+
+    $(document).click( function(){
+        $('.drop-menu').hide();
+    });
+
+    /**
+    * Fancybox
+    */
+    $(".fancybox").fancybox();
+});
+
+/**
+* Window scroll effects
+*/
+$(window).scroll(function(){
+    /**
+    * Scroll slow effect to #packages
+    */
+    $("#to-top").click(function() {
+        $('html:not(:animated),body:not(:animated)').animate({
+            scrollTop: $("#packages").offset().top
+        }, 2000);
+        return false;
+        $(window).bind("mousewheel", function() {
+        $("html, body").stop(true, false);
+    });
+    });
+
+    /**
+    * Scroll slow effect to #booking
+    */
+    $(".scroll-to").click(function() {
+        $('html:not(:animated),body:not(:animated)').animate({
+            scrollTop: $("#booking").offset().top
+        }, 2000);
+        return false;
+        $(window).bind("mousewheel", function() {
+        $("html, body").stop(true, false);
+        });
+    });
+});
+
+var headnav = document.getElementById('headnav');
+var headnavPos = headnav.offsetTop;
+window.onscroll = function() {
+    console.log(document.body.scrollTop);
+    if(document.body.scrollTop > headnavPos) {
+        if(headnav.style.position !== 'fixed') {
+            headnav.style.position = 'fixed';
+        }
+    } else  {
+        if(headnav.style.position === 'fixed') {
+            headnav.style.position = '';
+        }
+    }
+
+}
